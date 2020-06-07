3e7cf3c78570770258d4f110d6c9a5f56f037ef9
Arecost <znatokistoriy@gmail.com>
2020-06-07 12:27:24 +0200

    Add JS script

---
+++ 3.js
@@ -0,0 +1,48 @@
+function readMore() {
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btn = document.getElementById("btn");
+    
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        more.style.display = "none";
+        btn.innerHTML = "Read more";
+    } else {
+        dots.style.display = "none";
+        more.style.display = "inline";
+        btn.innerHTML = "Hide";
+    }
+};
+
+function readMore1() {
+    var dots = document.getElementById("dots1");
+    var more = document.getElementById("more1");
+    var btn = document.getElementById("btn1");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        more.style.display = "none";
+        btn.innerHTML = "Read more";
+    } else {
+        dots.style.display = "none";
+        more.style.display = "inline";
+        btn.innerHTML = "Hide";
+    }
+};
+
+function readMore2() {
+    var dots = document.getElementById("dots2");
+    var more = document.getElementById("more2");
+    var btn = document.getElementById("btn2");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        more.style.display = "none";
+        btn.innerHTML = "Read more";
+    } else {
+        dots.style.display = "none";
+        more.style.display = "inline";
+        btn.innerHTML = "Hide";
+    }
+};
